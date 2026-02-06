e4f61c8f632b972b8c0250f2fbf05d6a2b4b6bc6
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-06 18:32:18 +0000

    feat(users) : add consumed calories and exercise endpoints

--- backend/routes/bodyStats.routes.js
+++ backend/routes/bodyStats.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { getMe, updateBodyStats } from "../controllers/bodyStats.controller.js";
+import {
+  getMe,
+  updateBodyStats,
+  addConsumedCalories,
+  addExerciseCalories,
+  deleteConsumedCalories,
+  deleteExerciseCalories,
+} from "../controllers/bodyStats.controller.js";
 import { protect } from "../middleware/auth.js";
 const router = express.Router();
 
@@ -9,4 +16,11 @@ router.get("/me", protect, getMe);
 // PUT /api/users/update
 router.put("/update", protect, updateBodyStats);
 
+//Consumed Calrories
+router.post("/consumed-calories", protect, addConsumedCalories);
+router.delete("/consumed-calories/:id", protect, deleteConsumedCalories);
+
+//Exercise Calories
+router.post("/exercise-calories", protect, addExerciseCalories);
+router.delete("/exercise-calories/:id", protect, deleteExerciseCalories);
 export default router;

97984f928fb5b3c982ac91e0f34f141972db5de6
Sebastian Antonio <seb.antonio2004@gmail.com>
2026-02-08 13:03:33 +0000

    chore(ui) : styling

---
+++ frontend/src/components/userNavBar.jsx
@@ -0,0 +1,9 @@
+import "./userNavBar.css";
+export default function UserNavBar() {
+  return (
+    <div className="user-nav-bar">
+      <h1 className="user-nav-title">CALORIE TRACKER</h1>
+      <button></button>
+    </div>
+  );
+}
